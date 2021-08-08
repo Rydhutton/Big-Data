27c365f53a6799e818036c884b8a583179162749
Ryan <rydhutton@gmail.com>
2021-08-08 15:27:40 -0700

    Added Threading
    - Added threading to save user post and comment data of user.
    - Cleaned up code, removed unnecessary comments and tidied up the test functions.
    - Added limit to collect function so that you may specify a number of accounts to find before exiting the function.

--- collector.py
+++ collector.py
@@ -3,7 +3,8 @@ import re
 from datetime import datetime
 import mysql.connector
 from time import sleep
-#from threading import Thread
+#import threading
+from threading import Thread, active_count as active_thread_count
 
 #Need to implement Try-Catch once confirmed working
 
@@ -14,12 +15,14 @@ from time import sleep
 
 # make data in class object for simplicity sake
 
-GLOBAL_IPV4 = "192.168.1.101"
+GLOBAL_IPV4 = "localhost"
+
+# *************** OBJECT CLASSES ***************
 
 class User():
 
-    def __init__(self, username):
-        self.username = username
+    username: str
+    gender = age = agestamp = location = dob = None
 
     def __init__(self, username, age_gender=None, unix_time=None):
         self.username = username
@@ -28,8 +31,6 @@ class User():
             if unix_time is not None: 
                 self.agestamp = unix_utc_toString(unix_time)
                 self.initial_save()
-
-    gender = age = agestamp = location = dob = None
 
     def get_redditor(self):
         return login().redditor(self.username)
@@ -65,18 +66,12 @@ class User():
         if val is not None:
             self.agestamp = val
 
-    def save_user_to_db(self):
-        # IF EXISTS THEN UPDATE INFO
-        pass
-
     def save_comments_to_db(self):
         db = opendb()
         cur = db.cursor()
         redditor = self.get_redditor()
         count = 0
         for comment in redditor.comments.new(limit=1000):
-            print(type(comment))
-            print(count)
             count += 1
             sql_exec_commentdata(cur, comment)
         db.commit()
@@ -91,16 +86,26 @@ class User():
         db.commit()
         db.close()
 
+    def thread_save_to_db(self):
+        commentThread = Thread(target=self.save_comments_to_db)
+        postThread = Thread(target=self.save_posts_to_db)
+        # might remove daemon, need for testing now
+        commentThread.daemon = True
+        postThread.daemon = True
+        commentThread.start()
+        postThread.start()
+        commentThread.join()
+        postThread.join()
+
     def delete_from_db(self):
         if input("Are you sure? Press Y for yes").lower() == "y":
             dba = opendbAdmin()
             cur = dba.cursor()
             cur.execute("DELETE FROM userpostdata VALUES * WHERE username=%s",(self.username,))
-            cur.execute()
-            
-        pass
-        # Going to need to delete posts and comments first then delete
-        # the userdata
+            cur.execute("DELETE FROM usercommentdata VALUES * WHERE username=%s",(self.username,))
+            cur.execute("DELETE FROM userdata VALUES * WHERE username=%s",(self.username,))
+            dba.commit()
+            dba.close()
 
     def check_for_existing(self, username=None, cid=None, pid=None):
         booleans = []
@@ -119,6 +124,8 @@ class User():
         if pid is not None: booleans.append(search("SELECT pid FROM userpostdata WHERE pid=%s", pid))
         return booleans
 
+
+# **************** SQL DATABASE FUNCTIONS *****************
 
 def setAdminPassword(pwd):
     ADMIN_PASSWORD = pwd
@@ -179,13 +186,12 @@ def opendbAdmin(admin_password=None):
         )
     return db
 
-
 def backupdb():
     #backup sql database to local and remote repo
     pass
 
 def resetdb():
-    # Remakes the database, data will be lost
+    # Resets the database to a blank state, data will be lost
     db = opendbAdmin()
     cur = db.cursor()
     cur.execute("DROP TABLE IF EXISTS usercommentdata")
@@ -198,7 +204,7 @@ def resetdb():
     db.close()
 
 def cleandb(Redditor, admin_password=None):
-    # Use to delete any accounts in the database which have been deleted
+    # Use to delete any accounts in the database which have been deleted or suspended from reddit
     try:
         _ = Redditor.comment_karma
         return False
@@ -214,6 +220,36 @@ def cleandb(Redditor, admin_password=None):
         db.close()
         return True
 
+def sql_exec_userdata(cursor, user):
+    try:
+        sql = ("INSERT INTO userdata "
+                "(username, gender, age, agestamp) "
+                "VALUES (%s, %s, %s, %s)")
+        val = (user.username, user.gender, user.age, user.agestamp)
+        cursor.execute(sql, val)
+    except: pass
+
+def sql_exec_commentdata(cursor, comment):
+    try:
+        sql = ("INSERT INTO usercommentdata "
+                "(username, cid, comment, date) "
+                "VALUES (%s, %s, %s, %s)")
+        val = (comment.author.name, comment.id, comment.body, unix_utc_toString(comment.created_utc))
+        cursor.execute(sql, val)
+    except: pass
+
+def sql_exec_postdata(cursor, post):
+    try:
+        sql = ("INSERT INTO userpostdata "
+                "(username, pid, title, selftext, date) "
+                "VALUES (%s, %s, %s, %s, %s)")
+        val = (post.author.name, post.id, post.title, post.selftext, unix_utc_toString(post.created_utc))
+        cursor.execute(sql, val)
+    except: pass
+
+
+#********************** COLLECTOR FUNCTIONS ***********************
+
 def login():
     reddit = praw.Reddit("py-bot-master")
     return reddit
@@ -221,58 +257,167 @@ def login():
 def unix_utc_toString(unix_string):
     return datetime.utcfromtimestamp(unix_string).strftime('%Y-%m-%d %H:%M:%S')
 
-def sql_exec_userdata(cursor, user):
-    #(username VARCHAR(30) NOT NULL PRIMARY KEY, gender VARCHAR(15), age INT, agestamp DATETIME, location VARCHAR(95), dob DATE)
-    try:
-        sql = ("INSERT INTO userdata "
-                "(username, gender, age, agestamp) "
-                "VALUES (%s, %s, %s, %s)")
-        val = (user.username, user.gender, user.age, user.agestamp)
-        #print(user.username, user.gender, user.age, user.agestamp)
-        cursor.execute(sql, val)
-    except:
-        #print("Error inserting userdata\nKey: " + user.username)
-        pass
-
-def sql_exec_commentdata(cursor, comment):
-    #(username VARCHAR(30) NOT NULL, cid VARCHAR(15) NOT NULL PRIMARY KEY, comment TEXT, date DATETIME)
-    try:
-        sql = ("INSERT INTO usercommentdata "
-                "(username, cid, comment, date) "
-                "VALUES (%s, %s, %s, %s)")
-        val = (comment.author.name, comment.id, comment.body, unix_utc_toString(comment.created_utc))
-        #print(comment.author.name, comment.id, comment.body, unix_utc_toString(comment.created_utc))
-        cursor.execute(sql, val)
-    except:
-        #print("Error inserting commentdata\nKey: " + comment.id)
-        pass
-
-def sql_exec_postdata(cursor, post):
-    #(username VARCHAR(30) NOT NULL, pid VARCHAR(15) NOT NULL PRIMARY KEY, title TEXT, selftext TEXT, date DATETIME)
-    try:
-        sql = ("INSERT INTO userpostdata "
-                "(username, pid, title, selftext, date) "
-                "VALUES (%s, %s, %s, %s, %s)")
-        val = (post.author.name, post.id, post.title, post.selftext, unix_utc_toString(post.created_utc))
-        #print(post.author.name, post.id, post.title, post.selftext, unix_utc_toString(post.created_utc))
-        cursor.execute(sql, val)
-    except:
-        #print("Error inserting postdata\nKey: " + post.id)
-        pass
+def create_user(User):
+    # Create a method to thread the creation of a new user so that the main thread can keep
+    # monitoring the stream
+    pass
     
+def collect(reddit, limit=None):
+    if limit == None:
+        subreddit = reddit.subreddit("Relationships")
+        for submission in subreddit.stream.submissions():
+            title = submission.title.lower()
+            search = re.search("(i|my|i'm|i am|me) +\(([1-9][0-9][mf])\)", title)
+            if search:
+                target = User(submission.author.name, age_gender=search.group(2), unix_time=submission.created_utc)
+                target.save_comments_to_db()
+                target.save_posts_to_db()
+    else:
+        subreddit = reddit.subreddit("Relationships")
+        count = 0
+        for submission in subreddit.stream.submissions():
+            title = submission.title.lower()
+            search = re.search("(i|my|i'm|i am|me) +\(([1-9][0-9][mf])\)", title)
+            if search:
+                count += 1
+                target = User(submission.author.name, age_gender=search.group(2), unix_time=submission.created_utc)
+                print(target.username)
+                target.thread_save_to_db()
+            if count >= limit:
+                break
+        
+def prod_run():
+    collect(login(),limit=None)
 
-def collect(reddit):
-    subreddit = reddit.subreddit("Relationships")
-    for submission in subreddit.stream.submissions():
-        title = submission.title.lower()
-        search = re.search("(i|my|i'm|i am|me) +\(([1-9][0-9][mf])\)", title)
-        if search:
-            target = User(submission.author.name)#, search.group(2), submission.created_utc)
-            target.save_comments_to_db()
-            target.save_posts_to_db()
+# TESTS
+
+# TEST 0: Repetitive functions for testing purposes
+def test0_print_userdata(cur):
+    cur.execute("SELECT * FROM userdata")
+    x = cur.fetchall()
+    for i in x:
+        print(i)
+
+def test0_print_usercommentdata(cur):
+    cur.execute("SELECT * FROM usercommentdata")
+    x = cur.fetchall()
+    for i in x:
+        print(i)
+
+def test0_print_userpostdata(cur):
+    cur.execute("SELECT * FROM userpostdata")
+    x = cur.fetchall()
+    for i in x:
+        print(i)
+
+
+# TEST 1: Testing SQL database functionality
+
+def test1_0():
+    reddit = login()
+    db = opendb()
+    cur = db.cursor()
+    me = reddit.redditor("DesolateWolf")
+    sql_exec_userdata(cur, me.name)
+    for comment in me.comments.new(limit=10):
+        sql_exec_commentdata(cur, comment)
+    db.commit()
+    db.close()
+
+def test1_1():
+    db = opendb()
+    cur = db.cursor()
+    cur.execute("SELECT comment FROM usercommentdata WHERE username='DesolateWolf'")
+    vals = cur.fetchone()
+    while vals is not None:
+        print(vals)
+        vals = cur.fetchone()
+    resetdb()
+
+def test1_2():
+    db = opendb()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM userdata")
+    x = cur.fetchall()
+    for i in x:
+        print(i)
+    cur.execute("SELECT * FROM usercommentdata")
+    x = cur.fetchall()
+    for i in x:
+        print(i)
+
+# TEST 2: Testing User class functionality
+
+def test2_0():
+    me = User("DesolateWolf")
+    redditor = me.get_redditor()
+    print(redditor.name)
+    print("end")
+
+def test2_1():
+    resetdb()
+    reddit = login()
+    db = opendb()
+    cur = db.cursor()
+    collect(reddit, limit=100)
+    cur.execute("SELECT * FROM userdata")
+    vals = cur.fetchall()
+    for val in vals:
+        print(val)
+
+def test2_2():
+    u1 = User(username="2KareDogs")
+    u2 = User(username="ThisUserDoesNotExist")
+
+    val = u1.check_for_existing(username=u1.username)
+    print(val)
+    val = u1.check_for_existing(username=u1.username, cid="asdas", pid="asdas")
+    print(val)
+    val = u2.check_for_existing(username=u2.username)
+    print(val)
+
+# TEST 3: Testing database cleaning
+
+def test3_0():
+    pw = input("Admin password: ")
+    db = opendb()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM userdata")
+    dump = cur.fetchall()
+    accounts = []
+    for line in dump:
+        redditor = User(username=line[0])
+        redditor.gender = line[1]
+        redditor.age = line[2]
+        redditor.agestamp = line[3]
+        accounts.append(redditor)
+    for acc in accounts:
+        #sleep(1)
+        Redditor = acc.get_redditor()
+        if cleandb(Redditor, admin_password=pw): pass
+        else:
+            print(acc.username + " is a " + str(acc.age) + " " + acc.gender)
+            if Redditor.comment_karma is not None: print("they have " + str(Redditor.comment_karma) + " karma")
+
+# TEST 4: Testing threading
+
+def test4_0():
+    print(active_thread_count())
+
+def test4_1():
+    resetdb()
+    prod_run()
+
+
+# ******************** MAIN ********************
+# used to test for now
+
+if __name__ == "__main__":
+    test4_1()
 
 
 # CAN ONLY BE RUN IN PYTHON 3.10+
+# cmdline help menu with new search matching
 '''
 HELP_MENU = {
     "collect: collects incoming stream of users and stores them into userdata",
@@ -303,101 +448,3 @@ def cmdline():
         cmd = input(">")
         run_command(cmd)
 '''
-
-# TESTS
-
-def test1_1():
-    reddit = login()
-    db = opendb()
-    cur = db.cursor()
-    me = reddit.redditor("DesolateWolf")
-    sql_exec_userdata(cur, me.name)
-    for comment in me.comments.new(limit=10):
-        sql_exec_commentdata(cur, comment)
-    db.commit()
-    db.close()
-
-def test1_2():
-    db = opendb()
-    cur = db.cursor()
-    cur.execute("SELECT comment FROM usercommentdata WHERE username='DesolateWolf'")
-    vals = cur.fetchone()
-    while vals is not None:
-        print(vals)
-        vals = cur.fetchone()
-    resetdb()
-
-def test2_1():
-    resetdb()
-    reddit = login()
-    db = opendb()
-    cur = db.cursor()
-    data = collect(reddit, 100)
-    cur.execute("SELECT * FROM userdata")
-    vals = cur.fetchall()
-    for val in vals:
-        print(val)
-
-def test2_0():
-    me = User("DesolateWolf")
-    redditor = me.get_redditor()
-    print(redditor.name)
-    print("end")
-
-def test3_0():
-    db = opendb()
-    cur = db.cursor()
-    cur.execute("SELECT * FROM userdata")
-    x = cur.fetchall()
-    for i in x:
-        print(i)
-    cur.execute("SELECT * FROM usercommentdata")
-    x = cur.fetchall()
-    for i in x:
-        print(i)
-
-def test4_0():
-    pw = input("Admin password: ")
-    db = opendb()
-    cur = db.cursor()
-    cur.execute("SELECT * FROM userdata")
-    dump = cur.fetchall()
-    accounts = []
-    for line in dump:
-        redditor = User(username=line[0])
-        redditor.gender = line[1]
-        redditor.age = line[2]
-        redditor.agestamp = line[3]
-        accounts.append(redditor)
-    for acc in accounts:
-        #sleep(1)
-        Redditor = acc.get_redditor()
-        if cleandb(Redditor, admin_password=pw): pass
-        else:
-            print(acc.username + " is a " + str(acc.age) + " " + acc.gender)
-            if Redditor.comment_karma is not None: print("they have " + str(Redditor.comment_karma) + " karma")
-
-def test3_3():
-    u1 = User(username="2KareDogs")
-    u2 = User(username="ThisUserDoesNotExist")
-
-    val = u1.check_for_existing(username=u1.username)
-    print(val)
-    val = u1.check_for_existing(username=u1.username, cid="asdas", pid="asdas")
-    print(val)
-    val = u2.check_for_existing(username=u2.username)
-    print(val)
- 
-def scp_fix():
-    db = opendb()
-    cur = db.cursor()
-    
-
-# NEED TO COMPLETE ABOVE FUNCTION SO THAT IT PARSES CURRENT DATA
-# AND INSERTS ALL COMMENTS/POSTS FOR CURRENT USERS AND THEN
-# MODIFY COLLECT FUNCTION TO DO THIS ALL THE TIME
-# ADD IN THREADING?
-
-
-if __name__ == "__main__":
-    pass
