11568850b2ccf82768fab659d999dbc1e88a0ca4
Ryan <rydhutton@gmail.com>
2021-08-10 20:21:23 -0700

    Separated tests
    - Separated tests into it's own file tests.py
    - Cleaned and updated the cleandb function (untested)

--- collector.py
+++ collector.py
@@ -5,6 +5,8 @@ import mysql.connector
 from time import sleep
 from threading import Thread, active_count as active_thread_count
 
+from praw.reddit import Redditor
+
 #Praw data types and attributes: https://praw.readthedocs.io/en/latest/code_overview/praw_models.html
 #Spacy data types and attributes: https://spacy.io/api
 #MySQL python connector resources: https://dev.mysql.com/doc/connector-python/en/
@@ -98,9 +100,9 @@ class User():
         commentThread.join()
         postThread.join()
 
-    def delete_from_db(self):
+    def delete_from_db(self, admin_password=None):
         if input("Are you sure? Press Y for yes").lower() == "y":
-            dba = opendbAdmin()
+            dba = opendbAdmin(admin_password=admin_password)
             cur = dba.cursor()
             cur.execute("DELETE FROM userpostdata VALUES * WHERE username=%s",(self.username,))
             cur.execute("DELETE FROM usercommentdata VALUES * WHERE username=%s",(self.username,))
@@ -123,7 +125,8 @@ class User():
         if username is not None: booleans.append(search("SELECT username FROM userdata WHERE username=%s", username))
         if cid is not None: booleans.append(search("SELECT cid FROM usercommentdata WHERE cid=%s", cid))
         if pid is not None: booleans.append(search("SELECT pid FROM userpostdata WHERE pid=%s", pid))
-        return booleans
+        if len(booleans) > 1: return booleans
+        else: return booleans[0]
 
 
 # **************** SQL DATABASE FUNCTIONS *****************
@@ -206,22 +209,20 @@ def resetdb():
     db.commit()
     db.close()
 
-def cleandb(Redditor, admin_password=None):
+def cleandb():
     # Use to delete any accounts in the database which have been deleted or suspended from reddit
-    try:
-        _ = Redditor.comment_karma
-        return False
-    except:
-        print(Redditor.name)
-        if admin_password is None: db = opendbAdmin()
-        else: db = opendbAdmin(admin_password)
-        cur = db.cursor()
-        sql = """DELETE FROM userdata WHERE username=%s"""
-        val = Redditor.name
-        cur.execute(sql, (val,))
-        db.commit()
-        db.close()
-        return True
+    pw = input("Admin password: ")
+    db = opendb()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM userdata")
+    dump = cur.fetchall()
+    for line in dump:
+        user = User(username=line[0])
+        Redditor = user.get_redditor()
+        try:
+            _ = Redditor.comment_karma
+        except:
+            user.delete_from_db(admin_password=pw)
 
 def sql_exec_userdata(cursor, user):
     try:
@@ -262,11 +263,6 @@ def login():
 
 def unix_utc_toString(unix_string):
     return datetime.utcfromtimestamp(unix_string).strftime('%Y-%m-%d %H:%M:%S')
-
-def create_user(User):
-    # Create a method to thread the creation of a new user so that the main thread can keep
-    # monitoring the stream
-    pass
     
 def collect(reddit, limit=None):
     if limit == None:
@@ -345,132 +341,11 @@ def prod_run():
         if input("Would you like to continue and refresh stats? Y/N") == "N": exit_flag = True
 
 
-
-# TESTS
-
-# TEST 0: Repetitive functions for testing purposes
-def test0_print_userdata(cur):
-    cur.execute("SELECT * FROM userdata")
-    x = cur.fetchall()
-    for i in x:
-        print(i)
-
-def test0_print_usercommentdata(cur):
-    cur.execute("SELECT * FROM usercommentdata")
-    x = cur.fetchall()
-    for i in x:
-        print(i)
-
-def test0_print_userpostdata(cur):
-    cur.execute("SELECT * FROM userpostdata")
-    x = cur.fetchall()
-    for i in x:
-        print(i)
-
-
-# TEST 1: Testing SQL database functionality
-
-def test1_0():
-    reddit = login()
-    db = opendb()
-    cur = db.cursor()
-    me = reddit.redditor("DesolateWolf")
-    sql_exec_userdata(cur, me.name)
-    for comment in me.comments.new(limit=10):
-        sql_exec_commentdata(cur, comment)
-    db.commit()
-    db.close()
-
-def test1_1():
-    db = opendb()
-    cur = db.cursor()
-    cur.execute("SELECT comment FROM usercommentdata WHERE username='DesolateWolf'")
-    vals = cur.fetchone()
-    while vals is not None:
-        print(vals)
-        vals = cur.fetchone()
-    resetdb()
-
-def test1_2():
-    db = opendb()
-    cur = db.cursor()
-    cur.execute("SELECT * FROM userdata")
-    x = cur.fetchall()
-    for i in x:
-        print(i)
-    cur.execute("SELECT * FROM usercommentdata")
-    x = cur.fetchall()
-    for i in x:
-        print(i)
-
-# TEST 2: Testing User class functionality
-
-def test2_0():
-    me = User("DesolateWolf")
-    redditor = me.get_redditor()
-    print(redditor.name)
-    print("end")
-
-def test2_1():
-    resetdb()
-    reddit = login()
-    db = opendb()
-    cur = db.cursor()
-    collect(reddit, limit=100)
-    cur.execute("SELECT * FROM userdata")
-    vals = cur.fetchall()
-    for val in vals:
-        print(val)
-
-def test2_2():
-    u1 = User(username="2KareDogs")
-    u2 = User(username="ThisUserDoesNotExist")
-
-    val = u1.check_for_existing(username=u1.username)
-    print(val)
-    val = u1.check_for_existing(username=u1.username, cid="asdas", pid="asdas")
-    print(val)
-    val = u2.check_for_existing(username=u2.username)
-    print(val)
-
-# TEST 3: Testing database cleaning
-
-def test3_0():
-    pw = input("Admin password: ")
-    db = opendb()
-    cur = db.cursor()
-    cur.execute("SELECT * FROM userdata")
-    dump = cur.fetchall()
-    accounts = []
-    for line in dump:
-        redditor = User(username=line[0])
-        redditor.gender = line[1]
-        redditor.age = line[2]
-        redditor.agestamp = line[3]
-        accounts.append(redditor)
-    for acc in accounts:
-        #sleep(1)
-        Redditor = acc.get_redditor()
-        if cleandb(Redditor, admin_password=pw): pass
-        else:
-            print(acc.username + " is a " + str(acc.age) + " " + acc.gender)
-            if Redditor.comment_karma is not None: print("they have " + str(Redditor.comment_karma) + " karma")
-
-# TEST 4: Testing threading
-
-def test4_0():
-    print(active_thread_count())
-
-def test4_1():
-    resetdb()
-    prod_run()
-
-
 # ******************** MAIN ********************
-# used to test for now
+
 
 if __name__ == "__main__":
-  test4_1()
+  cleandb()
 
 
 # CAN ONLY BE RUN IN PYTHON 3.10+
